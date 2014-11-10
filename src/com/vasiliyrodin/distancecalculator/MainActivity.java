a1c5fe84ffb9c3939c91df4332cfc6e51351c978
VasiliyRodin <rodinvasiliy@gmail.com>
2014-11-09 16:38:33 -0800

    added overlay to show distance.

--- src/com/vasiliyrodin/distancecalculator/MainActivity.java
+++ src/com/vasiliyrodin/distancecalculator/MainActivity.java
@@ -9,6 +9,11 @@ import java.io.OutputStream;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Rect;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.hardware.Sensor;
@@ -24,6 +29,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.view.View;
 
 /**
@@ -50,15 +56,23 @@ public class MainActivity extends Activity implements SensorEventListener {
 
 	      @Override
 	      public void onPictureTaken(byte[] data, Camera camera) {
-
+	    
 	      Bitmap bitmap = BitmapFactory.decodeByteArray(data , 0, data .length);
 	      if(bitmap==null){
 	         Toast.makeText(getApplicationContext(), "not taken", Toast.LENGTH_SHORT).show();
 	      }
 	      else
 	      {
-	    	 saveBitMap(bitmap);
-	         Toast.makeText(getApplicationContext(), "taken", Toast.LENGTH_SHORT).show();    	
+	    	  android.graphics.Bitmap.Config bitmapConfig =
+	    		      bitmap.getConfig();
+	    		  // set default bitmap config if none
+	    		  if(bitmapConfig == null) {
+	    		    bitmapConfig = android.graphics.Bitmap.Config.ARGB_8888;
+	    		  }
+	    	  bitmap = bitmap.copy(bitmapConfig, true);
+	    	  drawText(bitmap,mDistanceText.getText().toString());
+	    	  saveBitMap(bitmap);
+	    	  Toast.makeText(getApplicationContext(), "taken", Toast.LENGTH_SHORT).show();    	
 	      }
 	      MainActivity.this.mPreview.startPreview();
 	   }
@@ -123,6 +137,32 @@ public class MainActivity extends Activity implements SensorEventListener {
     	    // TODO Auto-generated catch block
     	    e.printStackTrace();
     	}
+	}
+	
+	private void drawText(Bitmap bitmap, String text){
+ 
+		  Canvas canvas = new Canvas(bitmap);
+		  // new antialised Paint
+		  Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		  // text color - #3D3D3D
+		  paint.setColor(Color.WHITE);
+		  paint.setStyle(Style.FILL);
+		  // text size in pixels
+		  paint.setTextSize(bitmap.getHeight()/10);
+		  paint.setTextAlign(Paint.Align.RIGHT);
+		  // text shadow
+		  //paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
+		 
+		  // draw text to the Canvas center
+		  // Rect bounds = new Rect();
+		  // paint.getTextBounds(text, 0, text.length(), bounds);
+		  int x = bitmap.getWidth();
+		  int y = bitmap.getHeight();
+		 
+		  canvas.drawText(text, x, y, paint);
+		  
+		 
+
 	}
     
     
