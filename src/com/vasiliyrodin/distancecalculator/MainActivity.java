906c141f3ab9db94a7ac8388fd6a69beb9717717
VasiliyRodin <rodinvasiliy@gmail.com>
2014-11-27 12:36:25 -0800

    Revert "Revert "Refractor draw overlay.""
    This reverts commit 177492f04c315bda5187a29c0c80131f0f6fb277.

--- src/com/vasiliyrodin/distancecalculator/MainActivity.java
+++ src/com/vasiliyrodin/distancecalculator/MainActivity.java
@@ -42,10 +42,6 @@ import android.view.View;
  * @author Vasiliy
  *
  */
-/**
- * @author Vasiliy
- *
- */
 public class MainActivity extends Activity implements SensorEventListener {
 	private static final String HEIGHT = "Height";
 	private static final String TAG = "MainActivity";
@@ -84,12 +80,13 @@ public class MainActivity extends Activity implements SensorEventListener {
 	    		    bitmapConfig = android.graphics.Bitmap.Config.ARGB_8888;
 	    		  }
 	    	  bitmap = bitmap.copy(bitmapConfig, true);
-	    	  drawText(bitmap,mDistanceText.getText().toString());
+	    	  drawOverlay(bitmap);
 	    	  saveBitMap(bitmap);
 	    	  Toast.makeText(getApplicationContext(), "taken", Toast.LENGTH_SHORT).show();    	
 	      }
 	      MainActivity.this.mPreview.startPreview();
 	   }
+
 	};
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -168,16 +165,23 @@ public class MainActivity extends Activity implements SensorEventListener {
 		return "dc" + s + ".jpg";
 	}
 
-	private void drawText(Bitmap bitmap, String text){
+	private void drawOverlay(Bitmap bitmap) {
+		Canvas canvas = new Canvas(bitmap);
+		drawText(canvas,mDistanceText.getText().toString());
+		
+		
+	}
+
+	
+	private void drawText(Canvas canvas, String text){
  
-		  Canvas canvas = new Canvas(bitmap);
 		  // new antialised Paint
 		  Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		  // text color - #3D3D3D
 		  paint.setColor(Color.WHITE);
 		  paint.setStyle(Style.FILL);
 		  // text size in pixels
-		  paint.setTextSize(bitmap.getHeight()/10);
+		  paint.setTextSize(canvas.getHeight()/10);
 		  paint.setTextAlign(Paint.Align.RIGHT);
 		  // text shadow
 		  //paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
@@ -185,8 +189,8 @@ public class MainActivity extends Activity implements SensorEventListener {
 		  // draw text to the Canvas center
 		  // Rect bounds = new Rect();
 		  // paint.getTextBounds(text, 0, text.length(), bounds);
-		  int x = bitmap.getWidth();
-		  int y = bitmap.getHeight();
+		  int x = canvas.getWidth();
+		  int y = canvas.getHeight();
 		 
 		  canvas.drawText(text, x, y, paint);
 		  
